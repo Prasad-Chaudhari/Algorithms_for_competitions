4a604cdcd35e14e760137b3c6fd0696f43502486
Prasad8897 <codekarenge@gmail.com>
2017-05-27 10:17:18 +0530

    Changed to class Expo

--- pow_a_n_m.java
+++ pow_a_n_m.java
@@ -1,12 +1,26 @@
-public static long pow(long a,long n,long m){
-	if(n==1){
-		return a;
-	}
-	long p=pow(a,n/2,m);
-	if(n%2==0){
-		return (p*p)%m;
-	}
-	else{
-		return ((p*p)%m*a)%m;
-	}
+class Expo{
+    static long a_pow_n_mod_m(long a,long n,long m){
+        if(n==1){
+            return a;
+        }
+        long p=a_pow_n_mod_m(a,n/2,m);
+        if(n%2==0){
+            return (p*p)%m;
+        }
+        else{
+            return ((p*p)%m*a)%m;
+        }
+    }
+    static long a_pow_n(long a,long n){
+        if(n==1){
+            return a;
+        }
+        long p=a_pow_n(a,n/2);
+        if(n%2==0){
+            return (p*p);
+        }
+        else{
+            return ((p*p)*a);
+        }
+    }
 }
