1b8c95e17eb31ca38a19e69cc8808d14fef39be2
Prasad8897 <codekarenge@gmail.com>
2017-06-18 11:11:11 +0530

    Create Union_Find.java
    used in Kruskal's algorithm

---
+++ Union_Find.java
@@ -0,0 +1,38 @@
+class UnionFind{
+    int component[];
+    int size[];
+    int n;
+    ArrayList<LinkedList<Integer>> members;
+    public UnionFind(int p){
+        n=p;
+        component=new int[n+1];
+        size=new int[n+1];
+        members=new ArrayList<LinkedList<Integer>>();
+        for(int i=0;i<=n;i++){
+            component[i]=i;
+            size[i]=1;
+            members.add(new LinkedList<Integer>());
+            members.get(i).add(i);
+        }
+    }
+    
+    public int find(int k){
+        return component[k];
+    }
+    
+    public void union(int a,int b){
+        if(size[a]>size[b]){
+            a=a^b;
+            b=a^b;
+            a=a^b;
+        }
+        LinkedList<Integer> membersofa=members.get(a);
+        LinkedList<Integer> membersofb=members.get(b);
+        for(int i=0;i<size[a];i++){
+            int member=membersofa.removeFirst();
+            component[member]=component[b];
+            membersofb.add(member);
+        }
+        size[b]=size[b]+size[a];
+    }
+}
